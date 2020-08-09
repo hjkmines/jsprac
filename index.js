e83a5ef7224f3653af4220c40d89df87722e523c
Hyung Kim <hjkmines@gmail.com>
2020-08-09 00:21:14 -0600

    regex

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-const http = new HttpRequests 
-
-const data = [
-    name: 'John', 
-    username: 'johndoe', 
-    email: 'jdoe@gmail.com'
-]
-
-http.get('http://localhost:3000/teams')
-    .then(response => console.log(response))
-    .catch(err => console.log(err))
-
-http.post('http://localhost:3000/teams', data)
-    .then(response => console.log(response))
-    .catch(err => console.log(err))
-
-http.put('http://localhost:3000/teams/1', data)
-    .then(response => console.log(response))
-    .catch(err => console.log(err))
-
-http.delete('http://localhost:3000/teams/1')
-    .then(response => console.log(response)) 
-    .catch(err => console.log(err))
+try {
+    // Produce a ref error 
+    myFunction(); 
+} catch(err) {
+    console.log(err.message)
+} finally {
+    
+}
